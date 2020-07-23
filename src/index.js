b0b0a392c69cef5587006081ff531156a1eedc5f
BassV <khorotyanvahagn@gmail.com>
2020-07-23 23:26:02 +0400

    Library export fixed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Routes from "./Routes";
-
-import "./index.css";
-
-ReactDOM.render(
-  <React.StrictMode>
-    <Routes />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+export { default as SignIn } from "./components/SignIn";
+export { default as SignUp } from "./components/SignUp";
